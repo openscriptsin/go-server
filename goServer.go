88237313d5ca6f7ba7268a1219244d13b573068d
amir <amir.amiransari987@gmail.com>
2024-02-02 22:55:12 +0530

    add an interface and made struct private

--- goServer.go
+++ goServer.go
@@ -7,15 +7,20 @@ import (
 	"go.uber.org/dig"
 )
 
-type GinServer struct {
-	GinApp *gin.Engine
+type GinServer interface {
+	Start(string)
+	GetEngine() *gin.Engine
+}
+
+type ginServer struct {
+	ginApp *gin.Engine
 }
 
 // New returns a new GinServer instance
 // it take two parameter
 // *dig.Container
 // registerController is a function which help to load api endpoints/route
-func New(c *dig.Container, registerController func(*dig.Container)) *GinServer {
+func New(c *dig.Container, registerController func(*dig.Container)) GinServer {
 	// registering gin server instance to dig instance
 	// since this gin server instance is getting used in all the controllers to define routes
 	c.Provide(gin.New)
@@ -36,17 +41,22 @@ func New(c *dig.Container, registerController func(*dig.Container)) *GinServer {
 	// register controllers here because these are the entry point of app
 	registerController(c)
 
-	return &GinServer{GinApp: server}
+	return &ginServer{ginApp: server}
 }
 
 // Start the gin server
 // - port takes sting or if you want to use default port pass empty string
 // Default values
 // port - 8080
-func (g *GinServer) Start(port string) {
+func (g *ginServer) Start(port string) {
 
 	if port == "" {
 		port = "8080"
 	}
-	g.GinApp.Run(":" + port)
+	g.ginApp.Run(":" + port)
+}
+
+// return the instance of *gin.Engine
+func (g *ginServer) GetEngine() *gin.Engine {
+	return g.ginApp
 }
